4bb62fd438e8b9c7b53a265e732920bb0d255ff8
gugitgugit <rnwnsgur28@gmail.com>
2024-12-29 19:17:44 +0900

    style: 전역 스타일 추가가

--- src/GlobalStyles.jsx
+++ src/GlobalStyles.jsx
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    scrollbar-width: thin;
+  }
   body {
     font-family : pretendard;
   }
