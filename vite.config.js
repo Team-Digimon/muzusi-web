3a2e70dd018c377eca75fdab1383153a13d9003a
gugitgugit <rnwnsgur28@gmail.com>
2025-01-07 14:43:55 +0900

    docs: 불필요 주석 삭제제

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // 이 설정으로 로컬 네트워크의 IP 주소를 바인딩
-    port: 5173, // 사용할 포트 (기본값: 5173)
+    host: true,
+    port: 5173,
   },
 });
