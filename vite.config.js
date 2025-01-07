a98f93caebe1c8d09a52ebe59dd55d287ea16bab
gugitgugit <rnwnsgur28@gmail.com>
2025-01-07 14:34:03 +0900

    feat: 네트워크 환경 테스트를 위한 실행 옵션 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // 이 설정으로 로컬 네트워크의 IP 주소를 바인딩
+    port: 5173, // 사용할 포트 (기본값: 5173)
+  },
+});
