523bae36c5912254cbe7e5905469aa3b947ef4a7
gugitgugit <rnwnsgur28@gmail.com>
2025-01-08 14:57:35 +0900

    refact: 별도의 파일로 URL 관리

--- src/components/sign-in/SignInForm.jsx
+++ src/components/sign-in/SignInForm.jsx
@@ -1,16 +1,8 @@
 import styled from "styled-components";
 import MuzusiLogo from "../../assets/MuzusiLogo.png";
-import {
-  kakaoClientId,
-  kakaoRedirectUri,
-  naverClientId,
-  naverRedirectUri,
-} from "../../config/Env";
+import { kakaoLoginUrl, naverLoginUrl } from "../../config/Url";
 
 const SignInForm = () => {
-  const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoClientId}&redirect_uri=${kakaoRedirectUri}&response_type=code`;
-  const naverLoginUrl = `https://nid.naver.com/oauth2.0/authorize?client_id=${naverClientId}&redirect_uri=${naverRedirectUri}&response_type=code`;
-
   return (
     <SignInFormContainer>
       <SignInFormLogo href="/">
