56027e809ba8df59a14939fb0d77cefb4acea6fd
gugitgugit <rnwnsgur28@gmail.com>
2025-01-18 15:08:18 +0900

    feat: 에러 처리 로직 추가

--- src/api/authApi.jsx
+++ src/api/authApi.jsx
@@ -1,19 +1,40 @@
 import axios from "axios";
 import { baseUrl } from "@/config/Env";
+import { useAuth } from "@/contexts/useAuth";
 
 const authApi = axios.create({
   baseURL: baseUrl,
   withCredentials: true,
 });
 
-export const reissueAccessToken = async () => {
+export const reissueAccessToken = async (logout) => {
   try {
     const response = await authApi.get("/auth/reissue");
-    const { accessToken } = response.data.data;
-    localStorage.setItem("accessToken", accessToken);
-    return accessToken;
+    console.log(response);
+
+    if (response.data.code === 200) {
+      const { accessToken } = response.data.data;
+      localStorage.setItem("accessToken", accessToken);
+      console.log("토큰 재발급 성공", accessToken);
+      return accessToken;
+    }
+    throw new Error("토큰 재발급 오류");
   } catch (error) {
-    console.error("토큰 재발급 오류", error);
+    if (error.response) {
+      if (error.reponse.data.code === "0008") {
+        console.log(error.response.data.message);
+        alert("세션이 만료되었습니다. 다시 로그인해주세요.");
+        logout();
+        return null;
+      }
+      console.error(
+        "토큰 재발급 중 예상치 못한 오류",
+        error.response.data.message || error.message
+      );
+    } else {
+      console.error("네트워크 또는 서버 오류", error);
+    }
+
     throw error;
   }
 };
@@ -36,19 +57,17 @@ authApi.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config;
+    const { logout } = useAuth();
 
-    if (
-      error.response &&
-      error.response.status === 401 &&
-      !originalRequest._retry
-    ) {
+    if (error.response.data.code === "0004" && !originalRequest._retry) {
       originalRequest._retry = true;
       try {
-        const newAccessToken = await reissueAccessToken();
+        const newAccessToken = await reissueAccessToken(logout);
         authApi.defaults.headers["Authorization"] = `Bearer ${newAccessToken}`;
         return authApi(originalRequest);
       } catch (error) {
         console.error("토큰 재발급 실패", error);
+        logout();
         return Promise.reject(error);
       }
     }
