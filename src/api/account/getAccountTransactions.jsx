efb2141a70581854c9d1414f48e4497f5fe51618
Koo Jun Hyeok <164734665+gugitgugit@users.noreply.github.com>
2025-03-01 23:47:15 +0900

    Feature: 현재 계좌 거래 내역 구현 (#54)
    * feat: 거래 내역 API 연결
    
    * feat: 거래 내역 UI 구현
    
    * refact: 계좌 기록 제목 추가

---
+++ src/api/account/getAccountTransactions.jsx
@@ -0,0 +1,22 @@
+import authApi from "@/api/authApi";
+
+const getAccountTransactions = async (accountId) => {
+  try {
+    const response = await authApi.get(`accounts/${accountId}`);
+    return response.data;
+  } catch (error) {
+    console.error("API 요청 중 오류 발생", error);
+    if (error.response) {
+      console.error("응답 오류:", error.response.data);
+      throw error.response.data;
+    } else if (error.request) {
+      console.error("네트워크 오류 또는 서버 응답 없음");
+      throw new Error("네트워크 오류 또는 서버 응답 없음");
+    } else {
+      console.error("요청 설정 오류:", error.message);
+      throw new Error("요청 설정 오류");
+    }
+  }
+};
+
+export default getAccountTransactions;
---
+++ src/components/account/AccountTransactions.jsx
@@ -0,0 +1,322 @@
+import getAccountTransactions from "@/api/account/getAccountTransactions";
+import getCurrentAccount from "@/api/account/getCurrentAccount";
+import { useCallback, useEffect, useState } from "react";
+import styled from "styled-components";
+import Loading from "@/components/common/Loading";
+import Error from "@/components/common/Error";
+
+const AccountTransactions = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [tradeType, setTradeType] = useState("완료");
+  const [transactionDetail, setTransactionDetail] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const tradeTypes = ["완료", "예약"];
+
+  const handleTradeType = (type) => () => {
+    setTradeType(type);
+  };
+
+  const openDetail = (transaction) => () => {
+    setTransactionDetail((prev) =>
+      prev.id === transaction.id ? {} : transaction
+    );
+  };
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${month}. ${day}`;
+  };
+
+  const formatDateTime = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+
+    return `${year}년 ${month}월 ${day}일 ${hours}:${minutes}:${seconds}`;
+  };
+
+  const fetchAccountTransactions = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const currentAccount = await getCurrentAccount();
+      const response = await getAccountTransactions(currentAccount.data.id);
+      setTransactions(response.data);
+    } catch (error) {
+      console.error("계좌 거래 내역 가오 실패: ", error.message);
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchAccountTransactions();
+  }, [fetchAccountTransactions]);
+
+  if (isLoading) return <Loading />;
+  if (error) return <Error />;
+
+  return (
+    <AccountTransactionsContainer>
+      <Title>거래 내역</Title>
+      <TradeTypes>
+        {tradeTypes.map((type, index) => {
+          return (
+            <TradeType
+              key={index}
+              onClick={handleTradeType(type)}
+              $isActive={tradeType === type}
+            >
+              {type}
+            </TradeType>
+          );
+        })}
+      </TradeTypes>
+      {tradeType === "완료" && (
+        <TransactionsContainer>
+          <TransactionsInfo
+            $isDetailOpen={Object.keys(transactionDetail).length !== 0}
+          >
+            {transactions.map((transaction, index) => {
+              const formattedDate = formatDate(transaction.tradeAt);
+              const prevFormattedDate =
+                index > 0 ? formatDate(transactions[index - 1].tradeAt) : null;
+              const isSelected = transactionDetail.id === transaction.id;
+
+              return (
+                <Info
+                  key={transaction.id}
+                  onClick={openDetail(transaction)}
+                  $isSelected={isSelected}
+                >
+                  <TradeInfo>
+                    <TradeDate>
+                      {formattedDate !== prevFormattedDate && formattedDate}
+                    </TradeDate>
+                    <TradeBody>
+                      <StockName>{transaction.stockName}</StockName>
+                      <TransactionType $type={transaction.tradeType === "BUY"}>
+                        {transaction.tradeType === "BUY"
+                          ? "구매완료"
+                          : "판매완료"}
+                        <StockCount>
+                          &middot; {transaction.stockCount}주
+                        </StockCount>
+                      </TransactionType>
+                    </TradeBody>
+                  </TradeInfo>
+                  <StockPrice>
+                    {transaction.stockPrice.toLocaleString()}원
+                  </StockPrice>
+                </Info>
+              );
+            })}
+          </TransactionsInfo>
+          {Object.keys(transactionDetail).length !== 0 && (
+            <TransactionDetail>
+              <DetailStockName>{transactionDetail.stockName}</DetailStockName>
+              <DetailType>
+                {transactionDetail.tradeType === "BUY"
+                  ? "구매 완료"
+                  : "판매 완료"}
+              </DetailType>
+              <DetailLine>
+                <DetailLabel>주문 시간</DetailLabel>
+                <DetailInfo>
+                  {formatDateTime(transactionDetail.tradeAt)}
+                </DetailInfo>
+              </DetailLine>
+              <DetailLine>
+                <DetailLabel>1주 구매 가격</DetailLabel>
+                <DetailInfo>
+                  {transactionDetail.stockPrice.toLocaleString()}원
+                </DetailInfo>
+              </DetailLine>
+              <DetailLine>
+                <DetailLabel>구매 수량</DetailLabel>
+                <DetailInfo>{transactionDetail.stockCount}주</DetailInfo>
+              </DetailLine>
+              <DetailLine style={{ borderTop: "1px solid #001b371a" }}>
+                <DetailLabel>총 구매 금액</DetailLabel>
+                <DetailInfo>
+                  {(
+                    transactionDetail.stockPrice * transactionDetail.stockCount
+                  ).toLocaleString()}
+                  원
+                </DetailInfo>
+              </DetailLine>
+            </TransactionDetail>
+          )}
+        </TransactionsContainer>
+      )}
+    </AccountTransactionsContainer>
+  );
+};
+
+export default AccountTransactions;
+
+const AccountTransactionsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.div`
+  font-weight: 600;
+  font-size: 24px;
+  color: #333d4b;
+  line-height: 1.45;
+  margin-bottom: 24px;
+`;
+
+const TradeTypes = styled.div`
+  border-bottom: 1px solid #001b371a;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 48px;
+  gap: 20px;
+`;
+
+const TradeType = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 1.45;
+  cursor: pointer;
+  color: ${({ $isActive }) => ($isActive ? "#000" : "#4e5968")};
+  border-bottom: ${({ $isActive }) => ($isActive ? "2px solid #000" : "none")};
+`;
+
+const TransactionsContainer = styled.div`
+  display: flex;
+  min-height: 500px;
+`;
+
+const TransactionsInfo = styled.div`
+  display: flex;
+  padding-top: 12px;
+  padding-right: 12px;
+  width: ${({ $isDetailOpen }) =>
+    $isDetailOpen ? "calc(100% - 300px)" : "100%"};
+  flex-direction: column;
+  transition: 0.2s;
+`;
+
+const Info = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  border-radius: 8px;
+  padding: 8px 0px;
+  background-color: ${({ $isSelected }) =>
+    $isSelected ? "#f9fafb" : "transparent"};
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0220470d;
+  }
+`;
+
+const TradeDate = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 65px;
+  color: #333d4b;
+  line-height: 1.45px;
+  font-weight: 500;
+  font-size: 15px;
+`;
+
+const TradeBody = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StockName = styled.div`
+  font-weight: 600;
+  font-size: 15px;
+  color: #4e5968;
+  line-height: 1.45;
+`;
+
+const TransactionType = styled.div`
+  font-weight: 400;
+  font-size: 14px;
+  color: ${({ $type }) => ($type ? "#f04452" : "#3182f6")};
+  line-height: 1.45;
+`;
+
+const StockCount = styled.span`
+  font-weight: 400;
+  font-size: 14px;
+  color: #6b7684;
+  line-height: 1.45;
+`;
+
+const TradeInfo = styled.div`
+  display: flex;
+`;
+
+const StockPrice = styled.div`
+  font-weight: 600;
+  font-size: 15px;
+  color: #4e5968;
+  line-height: 1.45;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  padding-right: 20px;
+`;
+
+const TransactionDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-left: 1px solid #001b371a;
+  padding: 12px;
+  width: 300px;
+`;
+
+const DetailStockName = styled.div`
+  font-weight: 400;
+  font-size: 12px;
+  color: #4e5968;
+  line-height: 1.45;
+`;
+
+const DetailType = styled.div`
+  font-weight: 600;
+  font-size: 17px;
+  color: #191f28;
+  line-height: 1.45;
+  margin-bottom: 12px;
+`;
+
+const DetailLine = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 1.45;
+  padding: 8px 0px;
+`;
+
+const DetailLabel = styled.span`
+  color: #4e5968;
+`;
+
+const DetailInfo = styled.span`
+  color: #333d4b;
+`;
