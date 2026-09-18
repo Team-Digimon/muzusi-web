6357bfe1c6f18c2983e9aa50024c41b67af33587
구준혁 <rnwnsgur28@gmail.com>
2026-09-19 00:36:32 +0900

    feat: Sentry 초기화 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import App from "@/App";
 import AuthProvider from "@/contexts/AuthProvider";
 import queryClient from "@/api/queryClient";
+import { initSentry } from "@/config/Sentry";
+
+initSentry();
 
 // index.html에 정적으로 존재하는 엘리먼트라 실제로 null일 일은 없지만,
 // getElementById의 반환 타입은 HTMLElement | null이라 단언이 필요하다.
